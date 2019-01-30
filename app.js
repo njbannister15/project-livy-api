dc7ddd9f72133d7778108bc693d1cf785a9d31bd
Nicholas Bannister <njbannister15@gmail.com>
2019-01-30 01:25:05 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.json({
+        major: 1,
+        minor: 0,
+        build: "development"
+    })
+});
+app.use('/users', usersRouter);
+
+module.exports = app;
